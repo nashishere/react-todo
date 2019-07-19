23d633f89692a79e5faf8c5bad7b93a854878dc1
Serhat Cevik <blumen@gmail.com>
2019-07-19 15:04:38 +0200

    Checkpoint (Converted from Flow to TS - A Fix)

--- __mocks__/react-i18next.tsx
+++ __mocks__/react-i18next.tsx
@@ -1,17 +1,9 @@
 import * as React from 'react';
-import {
-  I18nextProvider,
-  initReactI18next,
-  setDefaults,
-  getDefaults,
-  setI18n,
-  getI18n,
-} from '../node_modules/react-i18next/src/index';
+import { getDefaults, getI18n, I18nextProvider, initReactI18next, setDefaults, setI18n } from 'react-i18next';
 
 /* eslint-disable */
 
 const hasChildren = node => node && (node.children || (node.props && node.props.children));
-
 const getChildren = node => (node && node.children ? node.children : node.props && node.props.children);
 
 const renderNodes = reactNodes => {
@@ -19,23 +11,27 @@ const renderNodes = reactNodes => {
     return reactNodes;
   }
 
-  return Object.keys(reactNodes).map((key, i) => {
+  const mapper = (key, i) => {
     const child = reactNodes[key];
     const isElement = React.isValidElement(child);
 
     if (typeof child === 'string') {
       return child;
     }
+
     if (hasChildren(child)) {
       const inner = renderNodes(getChildren(child));
       return React.cloneElement(child, { ...child.props, key: i }, inner);
     }
+
     if (typeof child === 'object' && !isElement) {
-      return Object.keys(child).reduce((str, childKey) => `${str}${child[childKey]}`, '');
+      return Object.keys(child).reduce((str, childKey): string => `${str}${child[childKey]}`, '');
     }
 
     return child;
-  });
+  };
+
+  return Object.keys(reactNodes).map(mapper);
 };
 
 const useMock = {
@@ -44,7 +40,7 @@ const useMock = {
 };
 
 module.exports = {
-  // withTranslation: () => (Component) => (props) => <Component t={k => k} {...props} />,
+  withTranslation: () => Component => props => <Component t={(k: string): string => k} {...props} />,
   Trans: ({ children }) => renderNodes(children),
   Translation: ({ children }) => children(k => k, { i18n: {} }),
   useTranslation: () => useMock,
